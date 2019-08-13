f0881494e216189cbcaa17f2ebd314f21e35599e
Danil <samylov5@mail.ru>
2019-08-13 18:59:41 +0500

    First block

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 'use strict';
 
-var money = 'Ваш бюджет на месяц?';
-var time = 'Введите дату в формате YYYY-MM-DD';
+let money = prompt("Ваш бюджет на месяц?", ''),
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
-var  expenses = {
-    'Оплата жилья' : '5000'
+let appData = {
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
 };
 
-var appData = {
-    budget: money,
-    timeData: time,
-    give: expenses,
-    //mbgive: optionalexpenses, 
-    //upmoney: income,
-    savings: false
-};
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
 
-alert('Бюджет за один день = 500');
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
 
+alert(appData.budget / 30);
 
