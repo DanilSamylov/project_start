db700f82549e70f3debfc4880db74f5c0fb622dc
Danil <samylov5@mail.ru>
2019-08-13 18:18:05 +0500

    Start

---
+++ script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var money = 'Ваш бюджет на месяц?';
+var time = 'Введите дату в формате YYYY-MM-DD';
+
+var  expenses = {
+    'Оплата жилья' : '5000'
+};
+
+var appData = {
+    budget: money,
+    timeData: time,
+    give: expenses,
+    //mbgive: optionalexpenses, 
+    //upmoney: income,
+    savings: false
+};
+
+alert('Бюджет за один день = 500');
+
+
